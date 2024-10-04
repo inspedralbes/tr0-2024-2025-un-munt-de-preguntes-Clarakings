415f89ac239cca7014298bb7a0e5a4e83992991b
clara <a23clareyuba@inspedralbes.cat>
2024-10-04 09:29:04 +0200

    mejora

--- proyecto_a/backend/conexio.php
+++ proyecto_a/backend/conexio.php
@@ -1,9 +1,9 @@
 <?php
 // Configuración de la base de datos
-$host = 'localhost'; // Cambia esto a tu host de la base de datos
-$usuario = 'root'; // Cambia esto a tu usuario
-$password = ''; // Añade aquí la contraseña si es necesario
-$base_datos = 'autoescuela'; // Cambia esto al nombre de tu base de datos
+$host = 'localhost';
+$usuario = 'root';
+$password = '';
+$base_datos = 'autoescuela'; 
 
 // Conectar a la base de datos
 $conn = new mysqli($host, $usuario, $password, $base_datos);
